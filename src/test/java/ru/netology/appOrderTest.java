3e12522c10bb682cbe00c047421ac607bfb92da6
abobus228 <sashasafronov767@gmail.com>
2025-06-17 23:16:08 +0300

    create test on form

--- src/test/java/ru/netology/appOrderTest.java
+++ src/test/java/ru/netology/appOrderTest.java
@@ -1,11 +1,10 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -24,6 +23,7 @@ public class appOrderTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -34,6 +34,13 @@ public class appOrderTest {
 
     @Test
     void shouldSendForm() {
-
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Сафронов Александр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79506106210");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        Assertions.assertTrue(result.isDisplayed());
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
+                result.getText().trim());
     }
 }
